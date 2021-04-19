f885d9b07fd2c4061ab0a017a4e093f5a74397d5
John Long <johnlonggately@gmail.com>
2021-04-19 16:39:03 +0000

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import css from './App.module.css';
+// import './App.css';
+import Sidebar from './components/Sidebar';
+
+function App() {
+  return (
+    <div className={css.App}>
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import css from './css/sidebar.module.css';
+
+const Sidebar = () =>  {
+    return (
+    <div className={css.sidebar}>
+        <a href = "#" rel="noreferrer" target = "_blank" alt = "a link">My Photos</a>
+        <a href = "#" rel="noreferrer" target = "_blank" alt = "a link">My Illustrations</a>
+        <a href = "#" rel="noreferrer" target = "_blank" alt = "a link">My Paintings</a>
+    </div>
+    )
+}
+
+export default Sidebar;
